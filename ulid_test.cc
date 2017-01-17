01de3cf711c47e81e24d630f1cb489ad40e57cdf
Suyash <dextrous93@gmail.com>
2017-01-17 10:45:56 +0530

    add Unmarshal and UnmarshalFrom

--- ulid_test.cc
+++ ulid_test.cc
@@ -4,7 +4,7 @@
 
 #include "ulid.hh"
 
-TEST(ulid, 1) {
+TEST(basic, 1) {
 	ulid::ULID ulid;
 	ulid::Encode(std::time(nullptr), []() { return 4; }, ulid);
 	std::string str = ulid::Marshal(ulid);
@@ -12,5 +12,16 @@ TEST(ulid, 1) {
 	ASSERT_EQ(26, str.size());
 	for (char c : str) {
 		ASSERT_NE(std::string::npos, std::string(ulid::Encoding).find(c));
+	}
+}
+
+TEST(Unmarshal, 1) {
+	ulid::ULID ulid = ulid::Unmarshal("0001C7STHC0G2081040G208104");
+
+	ulid::ULID ulid_expected;
+	ulid::Encode(1484581420, []() { return 4; }, ulid_expected);
+
+	for (int i = 0 ; i < 16 ; i++) {
+		ASSERT_EQ(ulid_expected.data[i], ulid.data[i]);
 	}
 }
